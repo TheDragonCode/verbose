12082bfbe8f0c2948f98e84373ae56a86416a1e7
Andrey Helldar <helldar@ai-rus.com>
2021-07-06 23:43:44 +0300

    Bump `andrey-helldar/support` minimum version to `^3.19|^4.0`

--- src/Facades/Log.php
+++ src/Facades/Log.php
@@ -2,13 +2,13 @@
 
 namespace Helldar\Verbose\Facades;
 
-use Helldar\Support\Facades\BaseFacade;
+use Helldar\Support\Facades\Facade;
 use Helldar\Verbose\Services\Logger;
 
 /**
  * @method static void write(string|array $messages)
  */
-final class Log extends BaseFacade
+final class Log extends Facade
 {
     protected static function getFacadeAccessor()
     {
